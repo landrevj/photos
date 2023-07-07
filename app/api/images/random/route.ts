3efa059d80fc834e56922e4d39064aba5e60dc60
Joseph Landreville <landrevillejoseph@gmail.com>
2023-07-07 15:06:57 -0700

    60 seconds

--- app/api/images/random/route.ts
+++ app/api/images/random/route.ts
@@ -12,10 +12,8 @@ export const GET = async () => {
   const images = await cursor.toArray();
   return NextResponse.json(images[0] || null, {
     headers: {
-      'Cache-Control': 'public, max-age=0, s-maxage=1, must-revalidate',
-      'CDN-Cache-Control': 'public, max-age=0, s-maxage=1, must-revalidate',
-      // 'Vercel-CDN-Cache-Control':
-      //   'public, max-age=0, s-maxage=1, must-revalidate',
+      'Cache-Control': 'public, max-age=0, s-maxage=60, must-revalidate',
+      'CDN-Cache-Control': 'public, max-age=0, s-maxage=60, must-revalidate',
     },
   });
 };
